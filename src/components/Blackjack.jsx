24ac93877ae1e841ab7dbcf21110ff10c56734e5
Liz Anderson <lizanderson.ca@gmail.com>
2023-09-10 13:50:08 -0700

    Draw single card for Player

--- src/components/Blackjack.jsx
+++ src/components/Blackjack.jsx
@@ -24,11 +24,22 @@ const Blackjack = () => {
                 setComputerHand([cards[2], cards[3]]);
             });
     };
+    const singleCardDraw = () => {
+        fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`)
+            .then((response) => response.json())
+            .then((data) => {
+                const newCard = data.cards[0];
+                setPlayerHand([...playerHand, newCard]);
+            });
+    };
 
     return (
         <div id="blackjack-board">
             <h1>Blackjack</h1>
             <button onClick={initialDeal}>Deal</button>
+            <button onClick={singleCardDraw}>
+                Draw Player Card
+            </button>
 
             <div id='deal-panel'>
                 <div>
