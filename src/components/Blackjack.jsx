9a96ac2737fe31a5cc817b262574e9e2bf13cfe4
Liz Anderson <lizanderson.ca@gmail.com>
2023-09-10 13:47:41 -0700

    Deal two cards each + styles

--- src/components/Blackjack.jsx
+++ src/components/Blackjack.jsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 
 const Blackjack = () => {
     const [deckId, setDeckId] = useState(null);
+    const [playerHand, setPlayerHand] = useState([]);
+    const [computerHand, setComputerHand] = useState([]);
 
     useEffect(() => {
         // initialize the game by creating a new deck
@@ -12,9 +14,41 @@ const Blackjack = () => {
             });
     }, []);
 
+    const initialDeal = () => {
+        // draw two cards for the player and computer
+        fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=4`)
+            .then((response) => response.json())
+            .then((data) => {
+                const cards = data.cards;
+                setPlayerHand([cards[0], cards[1]]);
+                setComputerHand([cards[2], cards[3]]);
+            });
+    };
+
     return (
         <div id="blackjack-board">
-            <h1>Blackjack {deckId}</h1>
+            <h1>Blackjack</h1>
+            <button onClick={initialDeal}>Deal</button>
+
+            <div id='deal-panel'>
+                <div>
+                    <h2>Player's Hand</h2>
+                    <div className="card-container player-hand">
+                        {playerHand.map((card, index) => (
+                            <img className="card" key={index} src={card.image} alt={card.code} />
+                        ))}
+                    </div>
+                </div>
+                <div>
+                    <h2>Computer's Hand</h2>
+                    <div className="card-container computer-hand">
+                        {computerHand.map((card, index) => (
+                            <img className="card" key={index} src={card.image} alt={card.code} />
+                        ))}
+                    </div>
+                </div>
+
+            </div>
         </div>
     );
 };
